62d8df196284c3419faf934ca09f3b4dc2870adb
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 19:57:27 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Преобразование десятичного числа в двоичное
+
+
+void Remainder(int a, int b)
+{
+    int[] array = new int[b];
+    int i = 0;
+    while (a > 0)
+    {
+        array[i] = a % 2;
+        i++;
+        a = a / 2;
+    }
+
+    for (int j = array.Length - 1; j >= 0; j--)
+    {
+        Console.Write($"{array[j]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+int mem = number;
+
+while (number > 0)
+{
+    number = number / 2;
+    count++;
+}
+
+Remainder(mem, count);
